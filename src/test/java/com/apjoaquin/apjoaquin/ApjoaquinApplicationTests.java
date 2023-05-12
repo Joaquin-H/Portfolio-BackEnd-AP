1120b43321385f9364caa468330d3c858fb41fae
JoaquinDeveloper <hernandezdeveloper@gmail.com>
2023-05-12 18:28:57 -0300

    DockerFile

--- src/test/java/com/apjoaquin/apjoaquin/ApjoaquinApplicationTests.java
+++ src/test/java/com/apjoaquin/apjoaquin/ApjoaquinApplicationTests.java
@@ -3,7 +3,7 @@ package com.apjoaquin.apjoaquin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ApjoaquinApplicationTests.class)
 class ApjoaquinApplicationTests {
 
 	@Test
