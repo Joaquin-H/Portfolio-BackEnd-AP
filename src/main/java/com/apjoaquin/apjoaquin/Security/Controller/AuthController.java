50b190627d6a848dd66880c9f3ddd4bb4fdf51f9
JoaquinDeveloper <hernandezdeveloper@gmail.com>
2023-05-13 12:11:48 -0300

    arreglo auth login

--- src/main/java/com/apjoaquin/apjoaquin/Security/Controller/AuthController.java
+++ src/main/java/com/apjoaquin/apjoaquin/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://portfolio-ap-fb.web.app")
+@CrossOrigin(origins = {"https://argpr-portfolio.web.app","http://localhost:4200"})
 public class AuthController {
 
     @Autowired
